c9aa4eca1fa130752064c2779d1674c16145e925
hayanLee <hayanlee.dev@gmail.com>
2024-06-26 12:20:32 +0900

    [240612] TypeScript(Modal)

--- 13_tsPractice/src/App.tsx
+++ 13_tsPractice/src/App.tsx
@@ -1,10 +1,10 @@
-import Counter from './components/Counter';
+import ModalControls from './components/Modal/ModalControls';
 
 function App() {
     return (
         <>
-            <Counter />
-            {/* <ModalControls /> */}
+            {/* <Counter /> */}
+            <ModalControls />
             {/* <QueryClientProvider client={queryClient}>
                 <PostForm />
                 <hr />
--- 13_tsPractice/src/components/Modal/Modal.context.tsx
+++ 13_tsPractice/src/components/Modal/Modal.context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { PropsWithChildren, createContext, useContext, useState } from 'react';
 
 type ModalContextValue = {
     // 둘은 반환값이 필요하지 않아
@@ -7,28 +7,27 @@ type ModalContextValue = {
 };
 
 // 초기값에도 명시적으로 넣어주기
-//초기값은 형태만 맞춰주면 됨
+// 초기값은 형태만 맞춰주면 됨 (빈 함수)
 const initialValue: ModalContextValue = { open: () => {}, close: () => {} };
 
+// 초기값을 넣어주면 자동으로 타입 추론이 되지만, 'useState'처럼 명시적으로 적어주는 것이 좋음
 // context Value의 형태를 제네릭으로 넣어주기
 // 이 제네릭은 반환하는 값과 같은 타입
 const ModalContext = createContext<ModalContextValue>(initialValue);
 
 export const useModal = () => useContext(ModalContext);
 
-export function ModalProvider({ children }: { children: React.ReactNode }) {
+export function ModalProvider({ children }: PropsWithChildren) {
+    // 리액트에서 아무것도 보여주지 않을때 컴포넌트에서는 null 리턴
+    // == 모달을 보여주거나 | 아무것도 보여주지 않거나
     const [modalElement, setModalElement] = useState<React.ReactElement | null>(null);
 
     // 객체 타입 이용하기
-    const open: ModalContextValue['open'] = (element) => {
-        setModalElement(element);
-    };
     // const open: (elemet: React.ReactElement) => void = (element) => {}; // 함수에 지정
     // const open = (elemet: React.ReactElement) => {}; // 매개변수에 타입 지정
+    const open: ModalContextValue['open'] = (element) => setModalElement(element);
+    const close: ModalContextValue['close'] = () => setModalElement(null);
 
-    const close: ModalContextValue['close'] = () => {
-        setModalElement(null);
-    }; // 객체 타입 이용하기
     return (
         <ModalContext.Provider value={{ open, close }}>
             {children}
@@ -41,6 +40,6 @@ export function ModalProvider({ children }: { children: React.ReactNode }) {
 // 초기값에서 함수를 넣을 때는 빈 함수 넣기
 // const modal = useModal();
 
-// 둘은 반환값이 필요하지 않아
+// 둘은 반환값이 필요하지 않아 (변수에 넣는 동작 없음)
 // modal.open(<SomeModal />);
 // modal.close();
